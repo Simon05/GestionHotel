a810ed08e87267673b988961609f43f8873f61e7
NADHIR <NADHIR@DESKTOP-T9N4LEV>
2017-02-16 15:16:09 +0100

    Remove Login

--- GestionHotel/GestionHotel/MainWindow.xaml.cs
+++ GestionHotel/GestionHotel/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GestionHotel
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
